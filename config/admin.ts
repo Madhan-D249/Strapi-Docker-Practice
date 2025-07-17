29e8171b57e91a612a5db62754b373bd3c934a2a
madhan-d249 <madhandeva249@gmail.com>
2025-07-17 17:10:55 +0530

    Completed part of the task

--- config/admin.ts
+++ config/admin.ts
@@ -1,20 +1,29 @@
-export default ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+export default ({ env }) => {
+  console.log('🔍 Checking ENV variables in admin.ts:', {
+    API_TOKEN_SALT: env('API_TOKEN_SALT'),
+    ADMIN_JWT_SECRET: env('ADMIN_JWT_SECRET'),
+    TRANSFER_TOKEN_SALT: env('TRANSFER_TOKEN_SALT'),
+    ENCRYPTION_KEY: env('ENCRYPTION_KEY'),
+  });
+
+  return {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
     },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
-});
+    apiToken: {
+      salt: env('API_TOKEN_SALT'),
+    },
+    transfer: {
+      token: {
+        salt: env('TRANSFER_TOKEN_SALT'),
+      },
+    },
+    secrets: {
+      encryptionKey: env('ENCRYPTION_KEY'),
+    },
+    flags: {
+      nps: env.bool('FLAG_NPS', true),
+      promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    },
+  };
+};
